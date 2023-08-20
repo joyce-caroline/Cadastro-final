3274a844ad58f4a24ca549806fac58016ebefda5
joyce-caroline <caroline.joyce5631@gmail.com>
2023-08-20 16:15:41 -0300

    refactor(Entrada de dados): Refatoração para uso do Map

--- cadastro.js
+++ cadastro.js
@@ -1,10 +1,38 @@
-function cadastrar(){
-    console.log("Calculando...");
+function cadastrar() {
+  console.log("Calculando...");
 
-    const codigo = (document.querySelector("#codigo").value).toUpperCase();
-    const produto = document.querySelector("#produto").value;
-    const quantidade = Number(document.querySelector("#quantidade").value);
-    const preco = Number(document.querySelector("#preco").value);
+  const dadosProduto = new Map();
 
-    console.log(codigo, produto, quantidade, preco);
+  //Entrada de Dados
+  dadosProduto.set(
+    "codigo",
+    document.querySelector("#codigo").value.toUpperCase()
+  );
+  dadosProduto.set("produto", document.querySelector("#produto").value);
+  dadosProduto.set(
+    "quantidade",
+    Number(document.querySelector("#quantidade").value)
+  );
+  dadosProduto.set("preco", Number(document.querySelector("#preco").value));
+
+  // console.log('codigo: ', dadosProduto.get('codigo'));
+  // console.log('produto: ', dadosProduto.get('produto'));
+  // console.log('quantidade: ', dadosProduto.get('quantidade'));
+  // console.log('preco: ', dadosProduto.get('preco'));
+  // console.log('total: ', dadosProduto.get('total'));
+
+  //Processamento
+  dadosProduto.set(
+    "total",
+    dadosProduto.get("quantidade") * dadosProduto.get("preco")
+  );
+
+  //Saída
+  document.querySelector("table").innerHTML += `<tr>
+    <td>${dadosProduto.get("codigo")}</td>
+    <td>${dadosProduto.get("produto")}</td>
+    <td>${dadosProduto.get("quantidade")}</td>
+    <td>${dadosProduto.get("preco")}</td>
+    <td>${dadosProduto.get("total")}</td>
+</tr>`;
 }
